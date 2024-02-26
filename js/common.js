56e8a265d7d442565214602d37615e58575764a7
zonocan <zonocan.y.m@icloud.com>
2024-02-26 15:02:39 +0900

    wp埋め込み状態 header

--- js/common.js
+++ js/common.js
@@ -80,15 +80,15 @@ window.addEventListener('scroll', function(){
 });
 // header
 
-const headerNavlnk = document.querySelectorAll('.pc-header-nav-link');
-for (let i = 0; i < headerNavlnk.length; i++) {
-  headerNavlnk[i].addEventListener("click", linkSwitch);
-}
+// const headerNavlnk = document.querySelectorAll('.pc-header-nav-link');
+// for (let i = 0; i < headerNavlnk.length; i++) {
+//   headerNavlnk[i].addEventListener("click", linkSwitch);
+// }
 
-function linkSwitch() {
-  document.querySelectorAll(".link-active")[0].classList.remove('link-active');
-  this.classList.add('link-active');
-}
+// function linkSwitch() {
+//   document.querySelectorAll(".link-active")[0].classList.remove('link-active');
+//   this.classList.add('link-active');
+// }
 
 
 const humbtn = document.querySelector('.hum-btn');
@@ -98,8 +98,9 @@ humbtn.addEventListener('click', () => {
   humbtn.classList.toggle('active');
 });
 
-// humlist.addEventListener('click', () => {
-//   slideToggle(humlist, 300);  
+// const humlist_a = document.querySelectorAll('.hum-list-accordion-contents li a');
+// humlist_a.addEventListener('click', () => {
+//   slideUp(humlist, 300);  
 //   humbtn.classList.toggle('active');
 // });
 // バーガーメニュー
@@ -119,12 +120,12 @@ hum_list_sitemap_recomend_accordion_triger_btn.addEventListener('click', () => {
   hum_list_sitemap_recomend_accordion_triger_btn.classList.toggle('active');
 });
 
-const hum_list_sitemap_bonus_accordion_triger_btn = document.querySelector('#hum-list-sitemap-bonus-accordion-triger-btn');
-const hum_list_sitemap_bonus_accordion_list = document.querySelector('#hum-list-sitemap-bonus-accordion-list');
-hum_list_sitemap_bonus_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(hum_list_sitemap_bonus_accordion_list, 300);  
-  hum_list_sitemap_bonus_accordion_triger_btn.classList.toggle('active');
-});
+// const hum_list_sitemap_bonus_accordion_triger_btn = document.querySelector('#hum-list-sitemap-bonus-accordion-triger-btn');
+// const hum_list_sitemap_bonus_accordion_list = document.querySelector('#hum-list-sitemap-bonus-accordion-list');
+// hum_list_sitemap_bonus_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(hum_list_sitemap_bonus_accordion_list, 300);  
+//   hum_list_sitemap_bonus_accordion_triger_btn.classList.toggle('active');
+// });
 
 const hum_list_sitemap_search_accordion_triger_btn = document.querySelector('#hum-list-sitemap-search-accordion-triger-btn');
 const hum_list_sitemap_search_accordion_list = document.querySelector('#hum-list-sitemap-search-accordion-list');
@@ -140,19 +141,19 @@ hum_list_sitemap_payment_accordion_triger_btn.addEventListener('click', () => {
   hum_list_sitemap_payment_accordion_triger_btn.classList.toggle('active');
 });
 
-const hum_list_sitemap_guide_accordion_triger_btn = document.querySelector('#hum-list-sitemap-guide-accordion-triger-btn');
-const hum_list_sitemap_guide_accordion_list = document.querySelector('#hum-list-sitemap-guide-accordion-list');
-hum_list_sitemap_guide_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(hum_list_sitemap_guide_accordion_list, 300);  
-  hum_list_sitemap_guide_accordion_triger_btn.classList.toggle('active');
-});
+// const hum_list_sitemap_guide_accordion_triger_btn = document.querySelector('#hum-list-sitemap-guide-accordion-triger-btn');
+// const hum_list_sitemap_guide_accordion_list = document.querySelector('#hum-list-sitemap-guide-accordion-list');
+// hum_list_sitemap_guide_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(hum_list_sitemap_guide_accordion_list, 300);  
+//   hum_list_sitemap_guide_accordion_triger_btn.classList.toggle('active');
+// });
 
-const hum_list_sitemap_event_accordion_triger_btn = document.querySelector('#hum-list-sitemap-event-accordion-triger-btn');
-const hum_list_sitemap_event_accordion_list = document.querySelector('#hum-list-sitemap-event-accordion-list');
-hum_list_sitemap_event_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(hum_list_sitemap_event_accordion_list, 300);  
-  hum_list_sitemap_event_accordion_triger_btn.classList.toggle('active');
-});
+// const hum_list_sitemap_event_accordion_triger_btn = document.querySelector('#hum-list-sitemap-event-accordion-triger-btn');
+// const hum_list_sitemap_event_accordion_list = document.querySelector('#hum-list-sitemap-event-accordion-list');
+// hum_list_sitemap_event_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(hum_list_sitemap_event_accordion_list, 300);  
+//   hum_list_sitemap_event_accordion_triger_btn.classList.toggle('active');
+// });
 
 const hum_list_sitemap_games_accordion_triger_btn = document.querySelector('#hum-list-sitemap-games-accordion-triger-btn');
 const hum_list_sitemap_games_accordion_list = document.querySelector('#hum-list-sitemap-games-accordion-list');
@@ -169,6 +170,52 @@ hum_list_relation_site_triger_btn.addEventListener('click', () => {
 });
 // バーガーリストアコーディオン
 
+const pc_header_nav_link_home = document.querySelector('#pc-header-nav-link-home');
+const header_hover_lists_home = document.querySelector('#header-hover-lists-home');
+pc_header_nav_link_home.addEventListener("mouseenter", () => {
+  slideDown(header_hover_lists_home, 300);
+});
+pc_header_nav_link_home.addEventListener("mouseleave", () => {
+  slideUp(header_hover_lists_home, 300);
+});
+
+const pc_header_nav_link_recomend = document.querySelector('#pc-header-nav-link-recomend');
+const header_hover_lists_recomend = document.querySelector('#header-hover-lists-recomend');
+pc_header_nav_link_recomend.addEventListener("mouseenter", () => {
+  slideDown(header_hover_lists_recomend, 300);
+});
+pc_header_nav_link_recomend.addEventListener("mouseleave", () => {
+  slideUp(header_hover_lists_recomend, 300);
+});
+
+const pc_header_nav_link_search = document.querySelector('#pc-header-nav-link-search');
+const header_hover_lists_search = document.querySelector('#header-hover-lists-search');
+pc_header_nav_link_search.addEventListener("mouseenter", () => {
+  slideDown(header_hover_lists_search, 300);
+});
+pc_header_nav_link_search.addEventListener("mouseleave", () => {
+  slideUp(header_hover_lists_search, 300);
+});
+
+const pc_header_nav_link_payment = document.querySelector('#pc-header-nav-link-payment');
+const header_hover_lists_payment = document.querySelector('#header-hover-lists-payment');
+pc_header_nav_link_payment.addEventListener("mouseenter", () => {
+  slideDown(header_hover_lists_payment, 300);
+});
+pc_header_nav_link_payment.addEventListener("mouseleave", () => {
+  slideUp(header_hover_lists_payment, 300);
+});
+
+const pc_header_nav_link_games = document.querySelector('#pc-header-nav-link-games');
+const header_hover_lists_games = document.querySelector('#header-hover-lists-games');
+pc_header_nav_link_games.addEventListener("mouseenter", () => {
+  slideDown(header_hover_lists_games, 300);
+});
+pc_header_nav_link_games.addEventListener("mouseleave", () => {
+  slideUp(header_hover_lists_games, 300);
+});
+// hover アコーディオン
+
 const footer_sitemap_home_accordion_triger_btn = document.querySelector('#footer-sitemap-home-accordion-triger-btn');
 const footer_sitemap_home_accordion_list = document.querySelector('#footer-sitemap-home-accordion-list');
 footer_sitemap_home_accordion_triger_btn.addEventListener('click', () => {
@@ -183,12 +230,12 @@ footer_sitemap_recomend_accordion_triger_btn.addEventListener('click', () => {
   footer_sitemap_recomend_accordion_triger_btn.classList.toggle('active');
 });
 
-const footer_sitemap_bonus_accordion_triger_btn = document.querySelector('#footer-sitemap-bonus-accordion-triger-btn');
-const footer_sitemap_bonus_accordion_list = document.querySelector('#footer-sitemap-bonus-accordion-list');
-footer_sitemap_bonus_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(footer_sitemap_bonus_accordion_list, 300);  
-  footer_sitemap_bonus_accordion_triger_btn.classList.toggle('active');
-});
+// const footer_sitemap_bonus_accordion_triger_btn = document.querySelector('#footer-sitemap-bonus-accordion-triger-btn');
+// const footer_sitemap_bonus_accordion_list = document.querySelector('#footer-sitemap-bonus-accordion-list');
+// footer_sitemap_bonus_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(footer_sitemap_bonus_accordion_list, 300);  
+//   footer_sitemap_bonus_accordion_triger_btn.classList.toggle('active');
+// });
 
 const footer_sitemap_search_accordion_triger_btn = document.querySelector('#footer-sitemap-search-accordion-triger-btn');
 const footer_sitemap_search_accordion_list = document.querySelector('#footer-sitemap-search-accordion-list');
@@ -204,19 +251,19 @@ footer_sitemap_payment_accordion_triger_btn.addEventListener('click', () => {
   footer_sitemap_payment_accordion_triger_btn.classList.toggle('active');
 });
 
-const footer_sitemap_guide_accordion_triger_btn = document.querySelector('#footer-sitemap-guide-accordion-triger-btn');
-const footer_sitemap_guide_accordion_list = document.querySelector('#footer-sitemap-guide-accordion-list');
-footer_sitemap_guide_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(footer_sitemap_guide_accordion_list, 300);  
-  footer_sitemap_guide_accordion_triger_btn.classList.toggle('active');
-});
+// const footer_sitemap_guide_accordion_triger_btn = document.querySelector('#footer-sitemap-guide-accordion-triger-btn');
+// const footer_sitemap_guide_accordion_list = document.querySelector('#footer-sitemap-guide-accordion-list');
+// footer_sitemap_guide_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(footer_sitemap_guide_accordion_list, 300);  
+//   footer_sitemap_guide_accordion_triger_btn.classList.toggle('active');
+// });
 
-const footer_sitemap_event_accordion_triger_btn = document.querySelector('#footer-sitemap-event-accordion-triger-btn');
-const footer_sitemap_event_accordion_list = document.querySelector('#footer-sitemap-event-accordion-list');
-footer_sitemap_event_accordion_triger_btn.addEventListener('click', () => {
-  slideToggle(footer_sitemap_event_accordion_list, 300);  
-  footer_sitemap_event_accordion_triger_btn.classList.toggle('active');
-});
+// const footer_sitemap_event_accordion_triger_btn = document.querySelector('#footer-sitemap-event-accordion-triger-btn');
+// const footer_sitemap_event_accordion_list = document.querySelector('#footer-sitemap-event-accordion-list');
+// footer_sitemap_event_accordion_triger_btn.addEventListener('click', () => {
+//   slideToggle(footer_sitemap_event_accordion_list, 300);  
+//   footer_sitemap_event_accordion_triger_btn.classList.toggle('active');
+// });
 
 const footer_sitemap_games_accordion_triger_btn = document.querySelector('#footer-sitemap-games-accordion-triger-btn');
 const footer_sitemap_games_accordion_list = document.querySelector('#footer-sitemap-games-accordion-list');
