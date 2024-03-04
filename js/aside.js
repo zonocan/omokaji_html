0c5d7fcd93b1c19c73282a0b25b34b03d21b8db1
zonocan <zonocan.y.m@icloud.com>
2024-03-05 01:43:07 +0900

    パンクズリストの実装とサイドナビボタンのjs変更

--- js/aside.js
+++ js/aside.js
@@ -3,6 +3,7 @@ const side_nav_btn = document.querySelector('.side-nav-btn');
 const side_nav = document.querySelector('.side-nav');
 const aside_close_btn = document.querySelector('.aside-close-btn');
 
+
 side_nav_btn.addEventListener('click', () => {
   side_nav.classList.toggle('active');
 });
@@ -13,11 +14,27 @@ aside_close_btn.addEventListener('click', () => {
 
 
 window.addEventListener('scroll', function(){
-    if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
-        side_nav_btn.classList.add('active');
+    //ドキュメントの高さ
+    const scrollHeight = document.body.clientHeight;
+
+    //スクロール位置
+    const scrollPosition = window.pageYOffset;
+
+    //windowの高さ
+    const windowHeignt = window.innerHeight;
+
+    const footer = document.querySelector('.footer');  
+
+    const footerHeight = footer.offsetHeight;
+    if(scrollHeight - scrollPosition - windowHeignt <= footerHeight){
+      side_nav_btn.classList.add('active');
     } else {
-        side_nav_btn.classList.remove('active');
-    }
+      side_nav_btn.classList.remove('active');
+    }    
+    
 });
 
+
+
+  
 // aside
